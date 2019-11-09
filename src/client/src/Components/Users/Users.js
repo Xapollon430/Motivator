a1e7b0f584dd506c3da40a43f788d13c5962b205
Vehbi K <vehbik@Vehbis-MacBook-Pro.local>
2019-11-09 00:51:40 -0500

    deploy colors

--- src/client/src/Components/Users/Users.js
+++ src/client/src/Components/Users/Users.js
@@ -12,7 +12,7 @@ const Users = ({
 	useEffect(() => {
 		const getUsers = async () => {
 			if (loggedIn) {
-				let usersResponse = await fetch("/users", {
+				let usersResponse = await fetch("https://ucsdashboard.herokuapp.com/users", {
 					headers: {
 						"Authorization": `Bearer ${JSON.parse(localStorage.getItem("jwtToken"))}`
 					}
