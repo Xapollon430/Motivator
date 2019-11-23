8311576e3fd388f81bbcc6c110663ab2624375c9
Vehbi K <vehbik@Vehbis-MacBook-Pro.local>
2019-11-22 23:58:10 -0500

    before prod

--- src/client/src/index.js
+++ src/client/src/index.js
@@ -2,9 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-fetch("https://ucsdashboard.herokuapp.com/cacheSales", {
-	method: "POST"
-});
-console.log(123);
-
 ReactDOM.render(<App />, document.getElementById("root"));
